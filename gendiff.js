820bf375b4489c4f1cec1aca8fc7aabbe59caa3c
folureen <folureen@gmail.com>
2025-12-19 20:36:19 +0300

    step-2-done

--- gendiff.js
+++ gendiff.js
@@ -6,9 +6,10 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .usage("[options]")
-  .option('-V', '--version', 'output the version number')
-  .helpOption('-h', '--help', 'display help for command')
+  .usage("[options] <filepath1> <filepath2>")
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'display help for command')
 
 
   program.parse();
